51671203a8c7a46330773a430184c19daad9db31
TseyiMayuku <mayukutseyi@gmail.com>
2023-06-28 13:42:06 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
-export default function getFullResponseFromAPI(Success) {
+export default function getFullResponseFromAPI(success) {
   const arr = new Promise((resolve, reject) => {
-    if (Success) {
+    if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
