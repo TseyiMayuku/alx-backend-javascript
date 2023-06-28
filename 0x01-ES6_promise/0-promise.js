c2b186c62f32a6171222372a74c5ffe7d97b59ed
TseyiMayuku <mayukutseyi@gmail.com>
2023-06-28 13:21:17 +0000

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,7 @@
-/* eslint-disable */
 export default function getResponseFromAPI() {
-  return (new Promise((resolve, reject) => {}));
+  const promise = new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
+  return promise;
 }
